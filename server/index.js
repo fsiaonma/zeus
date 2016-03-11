4ed75857020e402cacf15609f8500115273946a1
fsiaonma <sam.sin@vipshop.com>
2016-03-11 13:55:54 +0800

    Revert "【UPD】项目结构改造"
    This reverts commit a6bca4e4a2f73310f23f71b26d59838a7a744878.

--- server/index.js
+++ server/index.js
@@ -1,16 +1,40 @@
-var http = require('http');
-var https = require('https');
-var fs = require('fs');
+'use strict';
 
-var express = require('express');
-var app = express();
-app.set('port', process.env.PORT || 5000);
+var meta = require('../package.json'),
+    express = require('express'),
+    compress = require('compression'),
+    path = require('path'),
+    app = module.exports = express(),
+    middleware = ['combo', 'router', 'proxy', 'static', 'error'];
 
-// 设置路由
-app.use(require('./router')());
-
-// 创建 http 服务器
-app.listen(app.get('port'), function () {
-	console.log('[%s] Express server listening on port %d',
-	    app.get('env').toUpperCase(), app.get('port'));
+// lazy load middlewares
+middleware.forEach(function (m) {
+    middleware.__defineGetter__(m, function () {
+        return require('./' + m);
+    });
 });
+
+process.on('uncaughtException', function (err) {
+    (app.get('logger') || console).error('Uncaught exception:\n', err.stack);
+});
+
+app.set('name', meta.name);
+app.set('version', meta.version);
+app.set('port', process.env.PORT || 5000);
+app.set('root', path.resolve(__dirname, '../').replace(/\/+$/, ''));
+app.set('logger', console);
+app.enable('trust proxy');
+
+app.use(compress());
+app.use('/co', middleware.combo());
+app.use(middleware.router({index: '/' + meta.name + '/' + meta.version + '/index.html'}));
+// app.use('/api/*', middleware.proxy('http://cors-api-host'));
+app.use(middleware.static());
+app.use(middleware.error());
+
+if (require.main === module) {
+    app.listen(app.get('port'), function () {
+        console.log('[%s] Express server listening on port %d',
+            app.get('env').toUpperCase(), app.get('port'));
+    });
+}
